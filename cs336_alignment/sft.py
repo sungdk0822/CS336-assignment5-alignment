c4c3867764ad71995289541c79c696168a748d98
sungdk0822 <sungdk0822@naver.com>
2025-08-28 17:55:24 +0900

    implement get_response_log_probs function

--- cs336_alignment/sft.py
+++ cs336_alignment/sft.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
 
 
 # uv run pytest -k test_tokenize_prompt_and_output
@@ -74,6 +74,44 @@ def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     probabilities = stabilized_logits.exp() / stabilized_logits.exp().sum(dim=-1, keepdim=True)
 
     return -1 * (probabilities * probabilities.log()).sum(dim=-1)
+
+
+# uv run pytest -k test_get_response_log_probs
+def get_response_log_probs(
+    model: PreTrainedModel,
+    input_ids: torch.Tensor,
+    labels: torch.Tensor,
+    return_token_entropy: bool = False,
+) -> dict[str, torch.Tensor]:
+    '''
+    Args:
+        model: PreTrainedModel HuggingFace model used for scoring (placed on the correct device
+            and in inference mode if gradients should not be computed).
+        input_ids: torch.Tensor shape (batch_size, sequence_length), concatenated prompt +
+            response tokens as produced by your tokenization method.
+        labels: torch.Tensor shape (batch_size, sequence_length), labels as produced by your
+            tokenization method.
+        return_token_entropy: bool If True, also return per-token entropy by calling
+            compute_entropy.
+    Returns:
+        dict[str, torch.Tensor].
+            "log_probs" 
+                shape (batch_size, sequence_length), conditional log-probabilities
+                log pθ (xt | x<t).
+            "token_entropy" 
+                optional, shape (batch_size, sequence_length), per-token entropy
+                for each position (present only if return_token_entropy=True).
+    '''
+    logits = model(input_ids).logits
+    stabilized_logits = logits - logits.max(dim=-1, keepdim=True).values
+    log_probabilities = stabilized_logits - stabilized_logits.exp().sum(dim=-1, keepdim=True).log()
+    log_probabilities = torch.gather(log_probabilities, dim=2, index=labels.unsqueeze(-1)).squeeze(-1)
+    
+    result = {'log_probs': log_probabilities}
+    if return_token_entropy:
+        result['token_entropy'] = compute_entropy(logits)
+
+    return result
 
 
 if __name__ == '__main__':
